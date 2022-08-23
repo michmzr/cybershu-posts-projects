49325c3c580f2ee9b1102446bd961b42554b3812
Michał Mazur <michmzr@gmail.com>
2022-08-23 06:59:57 +0200

    Project initialization with spring initializer

---
+++ spring-boot-tests-separation/src/main/java/eu/cybershu/springboottestsseparation/SpringBootTestsSeparationApplication.java
@@ -0,0 +1,13 @@
+package eu.cybershu.springboottestsseparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestsSeparationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestsSeparationApplication.class, args);
+    }
+
+}
---
+++ spring-boot-tests-separation/src/test/java/eu/cybershu/springboottestsseparation/SpringBootTestsSeparationApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.cybershu.springboottestsseparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestsSeparationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
